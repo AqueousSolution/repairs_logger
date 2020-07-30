b52982504f11dd2fdbd678c33fb835d3478d97b3
Idahosa Akioya <akioyaidahosa@yahoo.com>
2020-07-30 03:28:54 +0100

    Updates

---
+++ precache-manifest.b1e27d96af640b459b80a96a06af7fc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf4a062b80bc690c890f053ff79b349",
+    "url": "/AqueousSolution/repairs_logger/index.html"
+  },
+  {
+    "revision": "bf3c9bd7e3f8ac1325dd",
+    "url": "/AqueousSolution/repairs_logger/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "86470a5e05305a8d4bd0",
+    "url": "/AqueousSolution/repairs_logger/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf3c9bd7e3f8ac1325dd",
+    "url": "/AqueousSolution/repairs_logger/static/js/2.663f3139.chunk.js"
+  },
+  {
+    "revision": "b95dc37c0ed71d54d96277a12db94c90",
+    "url": "/AqueousSolution/repairs_logger/static/js/2.663f3139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86470a5e05305a8d4bd0",
+    "url": "/AqueousSolution/repairs_logger/static/js/main.c5e365b3.chunk.js"
+  },
+  {
+    "revision": "d3ca6a270e7644259a99",
+    "url": "/AqueousSolution/repairs_logger/static/js/runtime-main.c6358727.js"
+  }
+]);
